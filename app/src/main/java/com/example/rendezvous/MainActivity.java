501fcb02e895d72c4959decd6428716aa9f0362c
LeonAlvarezz <107019128+LeonAlvarezz@users.noreply.github.com>
2023-04-08 09:47:01 +0700

    register update 1

--- app/src/main/java/com/example/rendezvous/MainActivity.java
+++ app/src/main/java/com/example/rendezvous/MainActivity.java
@@ -2,17 +2,20 @@ package com.example.rendezvous;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button goToButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Piseth Gay");
-        System.out.println("Agree");
-        System.out.println("Agree2");
-        System.out.println("punlue x2 gay");
+        goToButton = findViewById(R.id.goToButton);
+        goToButton.setOnClickListener(view ->{
+            Intent intent = new Intent(MainActivity.this, Register.class);
+            startActivity(intent);
+        });
     }
 }
