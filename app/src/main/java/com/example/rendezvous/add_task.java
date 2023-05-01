8b090cfb4021d90214e4d810a8779d6ced312307
vysedh2000 <hengvysedh@gmail.com>
2023-05-01 20:19:51 +0700

    Updated

--- app/src/main/java/com/example/rendezvous/add_task.java
+++ app/src/main/java/com/example/rendezvous/add_task.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class task_view extends AppCompatActivity {
+public class add_task extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_view);
+        setContentView(R.layout.activity_add_task);
     }
 }
--- app/src/main/java/com/example/rendezvous/update.java
+++ app/src/main/java/com/example/rendezvous/update.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class task_view extends AppCompatActivity {
+public class update extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_view);
+        setContentView(R.layout.activity_update);
     }
 }
